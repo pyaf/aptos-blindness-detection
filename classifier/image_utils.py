2e3637687d284b40e8fb23d374e6e021cb24b299
Rishabh Agrahari <rishabh.agrahari.eee15@iitbhu.ac.in>
2019-08-17 01:55:52 +0530

    "178_efficientnet-b5_f1_poma"

---
+++ classifier/image_utils.py
@@ -0,0 +1,121 @@
+import cv2
+import numpy as np
+
+def load_image(path, size):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (size, size))
+    return image
+
+
+def load_ben_color(path, size, sigmaX=10, crop=False):
+    """if crop=True: center crop retina"""
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    if crop:
+        image = crop_image_from_gray(image)
+    image = cv2.resize(image, (size, size))
+    image = cv2.addWeighted(image, 4, cv2.GaussianBlur(
+        image, (0, 0), sigmaX), -4, 128)
+    return image
+
+
+def load_ben_gray(path, size):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.resize(image, (size, size))
+    image = cv2.addWeighted(
+        image, 4, cv2.GaussianBlur(image, (0, 0), size / 10), -4, 128
+    )  # Ben Graham's preprocessing method [1]
+    # (IMG_SIZE, IMG_SIZE) -> (IMG_SIZE, IMG_SIZE, 3)
+    image = image.reshape(size, size, 1)
+    image = np.repeat(image, 3, axis=-1)
+    return image
+
+
+def crop_image_from_gray(img, tol=7):
+    if img.ndim == 2:
+        mask = img > tol
+        return img[np.ix_(mask.any(1), mask.any(0))]
+    elif img.ndim == 3:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        mask = gray_img > tol
+        check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
+        if check_shape == 0:  # image is too dark so that we crop out everything,
+            return img  # return original image
+        else:
+            img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
+            img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
+            img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
+            img = np.stack([img1, img2, img3], axis=-1)
+        return img
+
+
+def info_image(im):
+    # Compute the center (cx, cy) and radius of the eye
+    cy = im.shape[0]//2
+    midline = im[cy,:]
+    midline = np.where(midline>midline.mean()/3)[0]
+    if len(midline)>im.shape[1]//2:
+        x_start, x_end = np.min(midline), np.max(midline)
+    else: # This actually rarely happens p~1/10000
+        x_start, x_end = im.shape[1]//10, 9*im.shape[1]//10
+    cx = (x_start + x_end)/2
+    r = (x_end - x_start)/2
+    return cx, cy, r
+
+def resize_image(im, IMAGE_SIZE, augmentation=True):
+    # Crops, resizes and potentially augments the image to IMAGE_SIZE
+    cx, cy, r = info_image(im)
+    scaling = IMAGE_SIZE/(2*r)
+    rotation = 0
+    if augmentation:
+        scaling *= 1 + 0.3 * (np.random.rand()-0.5)
+        rotation = 360 * np.random.rand()
+    M = cv2.getRotationMatrix2D((cx,cy), rotation, scaling)
+    M[0,2] -= cx - IMAGE_SIZE/2
+    M[1,2] -= cy - IMAGE_SIZE/2
+    return cv2.warpAffine(im,M,(IMAGE_SIZE,IMAGE_SIZE)) # This is the most important line
+
+def subtract_median_bg_image(im):
+    k = np.max(im.shape)//20*2+1
+    bg = cv2.medianBlur(im, k)
+    return cv2.addWeighted (im, 4, bg, -4, 128)
+
+def subtract_gaussian_bg_image(im):
+    k = np.max(im.shape)/10
+    bg = cv2.GaussianBlur(im ,(0,0) ,k)
+    return cv2.addWeighted (im, 4, bg, -4, 128)
+
+def toCLAHEgreen(img):
+    clipLimit=2.0
+    tileGridSize=(8, 8)
+    img = np.array(img)
+    green_channel = img[:, :, 1]
+    clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=tileGridSize)
+    cla = clahe.apply(green_channel)
+    cla = clahe.apply(cla)
+    cla = np.expand_dims(cla, -1)
+    cla = np.repeat(cla, 3, -1)
+    return cla
+
+def id_to_image(path,
+        resize=True,
+        size=456,
+        augmentation=False,
+        subtract_gaussian=False,
+        subtract_median=False,
+        clahe_green=False
+    ):
+    im = cv2.imread(path)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    if resize_image:
+        im = resize_image(im, size, augmentation)
+    if subtract_gaussian:
+        im = subtract_gaussian_bg_image(im)
+    if subtract_median:
+        im = subtract_median_bg_image(im)
+    if clahe_green:
+        im = toCLAHEgreen(im)
+    return im
+
