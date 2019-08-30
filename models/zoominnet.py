dae47b210aaa669927fed332e6a7de9b136ef218
Rishabh Agrahari <rishabh.agrahari.eee15@iitbhu.ac.in>
2019-08-31 00:04:09 +0530

    "308_zoominnet_f1_ord"

--- models/zoominnet.py
+++ models/zoominnet.py
@@ -162,6 +162,7 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.do = nn.Dropout(0.2)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
@@ -219,6 +220,7 @@ class ResNet(nn.Module):
 
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
+        x = self.do(x)
         x = self.fc(x)
 
         return (x, y)
